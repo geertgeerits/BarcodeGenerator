234abb7c041e2d3b24a8effaa889c13ad0a1adf9
Geert Geerits <ggeerits@hotmail.com>
2022-10-24 08:43:38 +0200

    Update

--- BarcodeGenerator/MainPage.xaml.cs
+++ BarcodeGenerator/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 // Author ......: Geert Geerits - E-mail: geertgeerits@gmail.com
 // Copyright ...: (C) 2022-2022
 // Version .....: 1.0.18 Beta
-// Date ........: 2022-10-23 (YYYY-MM-DD)
+// Date ........: 2022-10-24 (YYYY-MM-DD)
 // Language ....: Microsoft Visual Studio 2022: .NET MAUI C# 10.0
 // Description .: Barcode Generator
 // Dependencies : NuGet Package: ZXing.Net.Maui by Redth version 0.2.0-preview.2 ; https://github.com/redth/ZXing.Net.Maui
