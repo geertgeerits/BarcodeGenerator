4f2190217e049092d99164f123b144a682e488b8
Geert Geerits <ggeerits@hotmail.com>
2023-12-04 19:14:05 +0100

    Update

--- BarcodeGenerator/Resources/Languages/CodeLang.Designer.cs
+++ BarcodeGenerator/Resources/Languages/CodeLang.Designer.cs
@@ -571,10 +571,11 @@ namespace BarcodeGenerator.Resources.Languages {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to ZXing.Net.Maui by Redth
-        ///Barcode scanning and generation for .NET MAUI applications.
+        ///   Looks up a localized string similar to Barcode scanning and generation for .NET MAUI applications.
         ///MIT License:
-        ///Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the &quot;Software&quot;), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the f [rest of string was truncated]&quot;;.
+        ///Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the &quot;Software&quot;), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+        ///
+        ///T [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string LicenseMit_Text {
             get {
