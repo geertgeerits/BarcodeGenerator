770151fdc79da253d0e69179ae3edf2717198850
Geert Geerits <ggeerits@hotmail.com>
2024-03-13 14:36:19 +0100

    Update

--- BarcodeGenerator/MainPage.xaml.cs
+++ BarcodeGenerator/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 // Author ......: Geert Geerits - E-mail: geertgeerits@gmail.com
 // Copyright ...: (C) 2022-2024
 // Version .....: 1.0.38
-// Date ........: 2024-02-08 (YYYY-MM-DD)
+// Date ........: 2024-03-13 (YYYY-MM-DD)
 // Language ....: Microsoft Visual Studio 2022: .NET 8.0 MAUI C# 12.0
 // Description .: Barcode Generator: ZXing - Barcode Scanner: Native Android and iOS.
 // Note ........: zxing:CameraBarcodeReaderView -> ex. WidthRequest="300" -> Grid RowDefinitions="400" (300 x 1.3333) = 3:4 aspect ratio
