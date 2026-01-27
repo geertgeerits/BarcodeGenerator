7ebb833caa3be71eaed4ca008f291c723156b87c
Geert Geerits <ggeerits@hotmail.com>
2026-01-27 17:47:30 +0100

    Update to version 1.0.46

--- BarcodeGenerator/MainPage.xaml.cs
+++ BarcodeGenerator/MainPage.xaml.cs
@@ -1,7 +1,7 @@
 ﻿/* Program .....: BarcodeGenerator.sln
  * Author ......: Geert Geerits - E-mail: geertgeerits@gmail.com
  * Copyright ...: (C) 2022-2026
- * Version .....: 1.0.45
+ * Version .....: 1.0.46
  * Date ........: 2026-01-27 (YYYY-MM-DD)
  * Language ....: Microsoft Visual Studio 2026: .NET 10.0 MAUI C# 14.0
  * Description .: Barcode Generator: ZXing - Barcode Scanner: Native Android and iOS
