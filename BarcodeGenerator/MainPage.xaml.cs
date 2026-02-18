eabaa6d0147b884329850b62bca8e2de97810418
Geert Geerits <ggeerits@hotmail.com>
2026-02-18 09:26:00 +0100

    Update

--- BarcodeGenerator/MainPage.xaml.cs
+++ BarcodeGenerator/MainPage.xaml.cs
@@ -2,7 +2,7 @@
  * Author ......: Geert Geerits - E-mail: geertgeerits@gmail.com
  * Copyright ...: (C) 2022-2026
  * Version .....: 1.0.47
- * Date ........: 2026-02-17 (YYYY-MM-DD)
+ * Date ........: 2026-02-18 (YYYY-MM-DD)
  * Language ....: Microsoft Visual Studio 2026: .NET 10.0 MAUI C# 14.0
  * Description .: Barcode Generator: ZXing - Barcode Scanner: Native Android and iOS
  * Note ........: Only portrait mode is supported for iOS (!!!BUG!!! problems with the editor in iOS when turning from landscape to portrait)
--- BarcodeGenerator/QrCodeHelper.cs
+++ BarcodeGenerator/QrCodeHelper.cs
@@ -10,22 +10,30 @@ namespace BarcodeGenerator
         /// </summary>
         /// <remarks>The QR code is generated with a fixed pixel size per module. If a logo is provided,
         /// it is scaled to 20% of the QR code's size and centered, with a border for improved visibility. The
-        /// returned ImageSource is suitable for use in UI frameworks that support image sources.</remarks>
+        /// returned ImageSource is suitable for use in UI frameworks that support image sources.
+        /// The ECC (Error Correction Code) levels for QR codes are as follows:
+        /// Level L: Corrects up to 7% of data damage.
+        /// Level M: Corrects up to 15% of data damage.
+        /// Level Q: Corrects up to 25% of data damage.
+        /// Level H: Corrects up to 30% of data damage.
+        /// The higher the ECC level, the more data can be recovered, but it also increases the size of the QR code.
+        /// </remarks>
         /// <param name="text">The text to encode within the generated QR code.</param>
         /// <param name="logoStream">A stream containing the logo image to overlay at the center of the QR code, or null to generate a QR code
         /// without a logo. The stream must be positioned at the beginning.</param>
         /// <returns>An ImageSource representing the generated QR code image, including the logo overlay if provided.</returns>
         public static ImageSource GenerateQrWithLogo(string text, Stream? logoStream)
         {
+            // Generate QR code data with high error correction level to allow for logo overlay
             var generator = new QRCodeGenerator();
-            var qrData = generator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            var qrData = generator.CreateQrCode(text, QRCodeGenerator.ECCLevel.H);
 
             var modules = qrData.ModuleMatrix;
             int moduleCount = modules.Count;
             int pixelsPerModule = 20;
             int size = moduleCount * pixelsPerModule;
 
-            using var bitmap = new SKBitmap(size, size);
+            using var bitmap = new SKBitmap(width: size, height: size);
             using var canvas = new SKCanvas(bitmap);
             canvas.Clear(SKColor.Parse(Globals.cCodeColorBg));
 
@@ -58,15 +66,16 @@ namespace BarcodeGenerator
                     float iconSize = size * 0.18f;                  // Scale logo to 18% of QR code size
                     float left = (size - iconSize) / 2f;
                     float top = (size - iconSize) / 2f;
-                    var dest = new SKRect(left, top, left + iconSize, top + iconSize);
+                    var dest = new SKRect(left: left, top: top, right: left + iconSize, bottom: top + iconSize);
 
                     // Optional: draw a border around the icon
                     var borderPaint = new SKPaint { Style = SKPaintStyle.Fill, Color = SKColor.Parse(Globals.cCodeColorBg) };
-                    float borderPadding = 6;
+                    float borderPadding = 4;
                     var borderRect = new SKRect(dest.Left - borderPadding, dest.Top - borderPadding,
                                                  dest.Right + borderPadding, dest.Bottom + borderPadding);
-                    canvas.DrawRoundRect(borderRect, 6, 6, borderPaint);
+                    canvas.DrawRoundRect(rect: borderRect, rx: 4, ry: 4, paint: borderPaint);
 
+                    // Draw the image centered on the QR code
                     canvas.DrawBitmap(logoBitmap, dest);
                 }
             }
