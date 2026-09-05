d5ec8d8175339b8ed8fd3dbf46c9b10e2a14f064
Geert Geerits <ggeerits@hotmail.com>
2026-09-05 16:17:48 +0200

    Update rMQR Code

--- BarcodeGenerator/ClassQRCodeScanning.cs
+++ BarcodeGenerator/ClassQRCodeScanning.cs
@@ -25,14 +25,49 @@ namespace BarcodeGenerator
         /// </summary>
         /// <param name="cImageFile"></param>
         /// <returns></returns>
-        /// <remarks>Works for standard QR code images but not for Micro QR codes and rectangular QR codes.</remarks>
         public static string QRCodeDecoderImage(string cImageFile)
         {
             using SKBitmap bitmap = SKBitmap.Decode(cImageFile);
             
             if (QRCodeDecoder.TryDecode(bitmap, out var decodedText, out var info))
             {
-                Debug.WriteLine($"Result decoder image: {decodedText} (version {info.Version}, ECC {info.EccLevel})");
+                Debug.WriteLine($"Result QR Code image: {decodedText} (version {info.Version}, ECC {info.EccLevel})");
+                return decodedText;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Decodes a Micro QR code from an image file and returns the decoded text.
+        /// </summary>
+        /// <param name="cImageFile"></param>
+        /// <returns></returns>
+        public static string MicroQRCodeDecoderImage(string cImageFile)
+        {
+            using SKBitmap bitmap = SKBitmap.Decode(cImageFile);
+
+            if (MicroQRCodeDecoder.TryDecode(bitmap, out var decodedText, out var info))
+            {
+                Debug.WriteLine($"Result Micro QR Code image: {decodedText} (version {info.Version}, ECC {info.EccLevel})");
+                return decodedText;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Decodes a Rectangular QR code from an image file and returns the decoded text.
+        /// </summary>
+        /// <param name="cImageFile"></param>
+        /// <returns></returns>
+        public static string RectangularQRCodeDecoderImage(string cImageFile)
+        {
+            using SKBitmap bitmap = SKBitmap.Decode(cImageFile);
+
+            if (RmQRCodeDecoder.TryDecode(bitmap, out var decodedText, out var info))
+            {
+                Debug.WriteLine($"Result rMQR Code image: {decodedText} (version {info.Version}, ECC {info.EccLevel})");
                 return decodedText;
             }
 
